52848ec7107fc5ef07d1ee5d97b339856c5f45f1
JuanseHenao <53921087+JuanseHenao@users.noreply.github.com>
2021-11-13 15:45:33 -0500

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require(`cors`)
+const cors = require('cors')
 const bodyparser = require('body-parser')
 
 app.use(morgan('dev'))
